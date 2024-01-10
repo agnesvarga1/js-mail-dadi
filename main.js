4b1b8c3252816c87716092f024886fc01c3f6a26
agnesvarga1 <agikolyok@gmail.com>
2024-01-10 16:45:14 +0100

    esercizio dadi log chi vince

--- main.js
+++ main.js
@@ -3,21 +3,32 @@
 
 //1) genera un numero1 random e salva
 //2) genera un numero2 random e salva
-//3) valuta se  num1 o num2 e maggiore
-//basando questo (quale e piu grande) declara un vincitore
-//condizione se i numeri sono uguali
 function generaRandomNumber(min, max) {
   let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNumber;
 }
+
+let randomNumberComputer = generaRandomNumber(1, 6);
+let randomNumberPlayer = generaRandomNumber(1, 6);
+console.log(`Player:${randomNumberPlayer}, Computer:${randomNumberComputer}`);
+//3) valuta se  num1 o num2 e maggiore
+//basando questo (quale e piu grande) declara un vincitore
+
+if (randomNumberComputer > randomNumberPlayer) {
+  console.log("computer vince");
+} else if (randomNumberComputer < randomNumberPlayer) {
+  console.log("hai vinto");
+  //condizione se i numeri sono uguali
+} else {
+  console.log("Parita");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
 //esercizio 2
 //mail
 
-//2)basando quel value controlla se esiste nel array
-//itera nel array con un ciclo for
-//se esiste risposta
-//se non esiste altro risposta
 //1)serve un array con gli indirizzi email
+
 const emailAddresses = [
   "john.doe@example.com",
   "jane.smith@example.com",
@@ -32,20 +43,25 @@ const emailAddresses = [
 ];
 const emailUserHtml = document.querySelector("#email-user");
 const checkEmailBtn = document.querySelector("#check-email");
-const displayResult = document.querySelector("#result-search");
+const mailMainContainer = document.querySelector(".esercizio-mail");
 //2)chiedi utente un email
 
 checkEmailBtn.addEventListener("click", function () {
   let userEmail = emailUserHtml.value;
   let mailEsiste = false;
+  //2)basando quel value controlla se esiste nel array
+  //itera nel array con un ciclo for
   for (let i = 0; i < emailAddresses.length; i++) {
     if (userEmail == emailAddresses[i]) {
       mailEsiste = true;
     }
   }
+
+  //se esiste risposta
   if (mailEsiste === true) {
-    console.log("esiste");
+    mailMainContainer.innerHTML += `<p style="color:green">Questo email esite nella lista</p>`;
+    //se non esiste altro risposta
   } else {
-    console.log("non esiste");
+    mailMainContainer.innerHTML += `<p style="color:red">Questo email non esite nella lista</p>`;
   }
 });
